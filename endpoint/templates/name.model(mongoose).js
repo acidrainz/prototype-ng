6760027794103e2db4e71064a843ea4ccc5e6d90
Ian Pansensoy <ian.pansensoy@nuworks.ph>
2015-08-05 16:40:55 -0700

    added auto increment

--- endpoint/templates/name.model(mongoose).js
+++ endpoint/templates/name.model(mongoose).js
@@ -1,12 +1,20 @@
 'use strict';
-
+<%= _.slugify(appname) %> - dev
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var <%= classedName %>Schema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  Schema = mongoose.Schema,
+  autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(mongoose);
+var <%= classedName %>
+Schema = new Schema({
+  str.toLowerCase(<%= classedName %>
+      _id): {
+      type: Schema.Types.ObjectId,
+      ref: '<%= classedName %>'
+    },
+    name: String,
+    info: String,
+    active: Boolean
 });
 
-module.exports = mongoose.model('<%= classedName %>', <%= classedName %>Schema);
+module.exports = mongoose.model('<%= classedName %>', <%= classedName %>
+  Schema);
