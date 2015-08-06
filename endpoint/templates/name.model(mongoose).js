8690081c547d3e07413c4ed668af0f1672c6a1ee
Ian Pansensoy <ian.pansensoy@nuworks.ph>
2015-08-05 17:22:28 -0700

    monngose auto increment on api generator

--- endpoint/templates/name.model(mongoose).js
+++ endpoint/templates/name.model(mongoose).js
@@ -1,20 +1,30 @@
 'use strict';
-<%= _.slugify(appname) %> - dev
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema,
   autoIncrement = require('mongoose-auto-increment');
-autoIncrement.initialize(mongoose);
-var <%= classedName %>
-Schema = new Schema({
-  str.toLowerCase(<%= classedName %>
-      _id): {
-      type: Schema.Types.ObjectId,
-      ref: '<%= classedName %>'
-    },
+  autoIncrement.initialize(mongoose);
+
+var prop = {
     name: String,
     info: String,
     active: Boolean
-});
+};
 
-module.exports = mongoose.model('<%= classedName %>', <%= classedName %>
-  Schema);
+var key  = '<%= classedName %>'.concat('_id').toLowerCase();
+      prop [key] = {
+        type: Schema.Types.ObjectId,
+        ref: '<%= classedName %>'
+      };
+
+var <%= classedName %>Schema = new Schema(prop);
+
+<%= classedName %>Schema.plugin(autoIncrement.plugin,
+              {model:'<%= classedName %>',
+                field : key.toString(),
+                startAt:1
+
+              }
+  );
+
+
+module.exports = mongoose.model('<%= classedName %>', <%= classedName %>Schema);
